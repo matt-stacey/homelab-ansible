20f0eb0b7e174716f80a27f4064e4ab4616c059b
matt@pixel7 <matt@pixel7>
2024-10-25 18:59:10 -0500

    update api_user by arg

--- prepare_proxmox_host.sh
+++ prepare_proxmox_host.sh
@@ -1,3 +1,30 @@
+#! /usr/bin/bash
+
+# Parse arguments
+api_user=
+while (( $# )); do
+    case $1 in
+        --api-user)
+            api_user=$1
+            ;;
+        --*)
+            echo 'bad option: '$1
+            ;;
+        *)
+            echo $1' not parsed'
+            ;;
+    esac
+    shift
+done
+
+
+# Replace existing_user with new_user in the file
+if [ $api_user ]; then
+    file="group_vars/all.yaml"
+    sed -i "s/api_user: .* #/api_user: $api_user #/" "$file"
+fi
+
+
 # Ansible Preparation - Proxmox Host
 ansible -u root proxmox -m ping
 ansible-playbook playbooks/prepare_proxmox_host.yaml
